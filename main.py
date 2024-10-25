a84dff9f66805babd68660b2affbf8dc0cc306f5
yasminebosle <fareedahusman@ryanacademy.shc.ng>
2024-10-25 14:44:13 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+string = input("Enter your own Sring")
+
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print("\nThe Original String = ", string)
+print("The Reserved String = ", string2)
